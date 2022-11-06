a7cf8226f5cd9889ef656541a3a5e4d52e423653
Dimaster <gordeev.dmitriy@physics.msu.ru>
2022-11-06 20:43:34 +0300

    refactoring

--- test/test_async_example.cpp
+++ test/test_async_example.cpp
@@ -1,45 +1,39 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
+#include <chrono>
+#include <thread>
+
 #include <iostream>
 #include <future>
 using std::cout;
 
 
-//int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
-
-//TEST_CASE("testing the factorial function") {
-//    CHECK(factorial(1) == 1);
-//    CHECK(factorial(2) == 2);
-//    CHECK(factorial(3) == 6);
-//    CHECK(factorial(10) == 3628800);
-//}
-
-
 int foo(int a) {
     cout << "foo() starts\n";
-    for (int i = 0; i < 100000; i++)
-        for (int j = 0; j < 100000; j++)
-            a = j + 2;
+
+    // assume some code is running in meantime
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+
     cout << "foo() ends\n";
+
     return a + 2;
 }
 
 
 TEST_CASE("simple async usage") {
 
+    // Create two async tasks and run them (saving into array arr)
     auto* arr = new std::future<int>[2];
     arr[0] = std::async(std::launch::async, &foo, 5);
     arr[1] = std::async(std::launch::async, &foo, 8);
 
-    cout << "main() line 1\n";
+    cout << "started 2 async tasks\n";
 
-    int b = 10;
-    for (int i = 0; i < 100000; i++)
-        for(int j = 0; j < 25000; j++)
-            b = 10;
+    // assume some code is running in meantime
+    std::this_thread::sleep_for(std::chrono::seconds(3));
 
-    cout << "main() line 2\n";
+    cout << "sleep ends\n";
 
 
     // This is example how we can wait for all futures to be finished
@@ -58,13 +52,13 @@ TEST_CASE("simple async usage") {
 
     CHECK(all_ready == true);
 
-    cout << "allReady = " << all_ready << "\n";
+    cout << "all_ready = " << all_ready << "\n";
 
     int value1 = arr[0].get();        // blocks until future is done
     int value2 = arr[1].get();
 
-    cout << "main() value1 = " << value1 << "\n";
-    cout << "main() value2 = " << value2 << "\n";
+    cout << "value1 = " << value1 << "\n";
+    cout << "value2 = " << value2 << "\n";
 
     delete[] arr;
 }
